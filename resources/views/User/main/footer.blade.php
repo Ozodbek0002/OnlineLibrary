724da75248278ebac2ab48475fe0439ed2e5d115
Ozodbek <ozodbekozodov0002@gmail.com>
2023-02-16 23:40:01 +0500

    template

---
+++ resources/views/User/main/footer.blade.php
@@ -0,0 +1,14 @@
+
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="inner-content">
+                    <p>Copyright &copy; 2022 Online Library.
+                        - Design: <a rel="nofollow noopener" href="https://t.me/Ozodbek_Ozodov2" target="_blank">Ozodbek Ozodov</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/User/main/nav.blade.php
@@ -0,0 +1,41 @@
+
+<!-- ***** Preloader Start ***** -->
+<div id="preloader">
+    <div class="jumper">
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+</div>
+<!-- ***** Preloader End ***** -->
+
+<!-- Header -->
+<header class="">
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="index.html"><h2>Sixteen <em>Clothing</em></h2></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{url('/')}}">Home
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="products.html">Our Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.html">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.html">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+
