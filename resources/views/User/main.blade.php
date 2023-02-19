390e84f6d520606b6a847915179149884ae929fb
Ozodbek <ozodbekozodov0002@gmail.com>
2023-02-19 21:41:03 +0500

    ...

--- resources/views/User/main.blade.php
+++ resources/views/User/main.blade.php
@@ -7,20 +7,20 @@
         <div class="owl-banner owl-carousel">
             <div class="banner-item-01">
                 <div class="text-content">
-                    <h4>Best Offer</h4>
-                    <h2>New Arrivals On Sale</h2>
+                    <h4>Eng yaxshi taklif</h4>
+                    <h2>Yangi kelganlar sotuvda</h2>
                 </div>
             </div>
             <div class="banner-item-02">
                 <div class="text-content">
-                    <h4>Flash Deals</h4>
-                    <h2>Get your best products</h2>
+                    <h4>Halol takliflar</h4>
+                    <h2>Eng sara kitoblarni oling</h2>
                 </div>
             </div>
             <div class="banner-item-03">
                 <div class="text-content">
-                    <h4>Last Minute</h4>
-                    <h2>Grab last minute deals</h2>
+                    <h4>Mahsus imkoniyat</h4>
+                    <h2>Hattoki ijaraga ham olishingiz mumkin</h2>
                 </div>
             </div>
         </div>
