bb1060cd5c3524254acb649192784db9d80ae0dc
Ozodbek <ozodbekozodov0002@gmail.com>
2023-02-20 22:28:38 +0500

    ...

--- resources/views/Admin/messages/index.blade.php
+++ resources/views/Admin/messages/index.blade.php
@@ -2,6 +2,92 @@
 @section('content')
 
 
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+
+
+                <hr>
+
+                <div class="card-body">
+
+                    <table class="table table-bordered text-center">
+                        <thead>
+                        <tr>
+                            <th class="" scope="col">T/R</th>
+                            <th class="" scope="col"> Mavzusi </th>
+                            <th class="" scope="col"> Muallifi </th>
+                            {{--                            <th class="" scope="col"> Kategoriya</th>--}}
+                            {{--                            <th class="" scope="col"> Muqova</th>--}}
+                            {{--                            <th class="" scope="col"> Narxi</th>--}}
+                            {{--                            <th class="" scope="col"> Kunlik</th>--}}
+                            {{--                            <th class="" scope="col"> Beti</th>--}}
+                            <th class="" scope="col"> Holat </th>
+                            <th class="" scope="col">Amallar</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        @foreach($messages as $ind=>$poet)
+                            <tr>
+                                <td class="col-1">{{($books->currentpage()-1)*($books->perpage())+$ind+1}}</td>
+                                <td>{!! $poet->title  !!}</td>
+                                <td>{!! $poet->author !!}</td>
+                                {{--                                <td>{!! $poet->category->name !!}</td>--}}
+                                {{--                                <td>{!! $poet->cover->name !!}</td>--}}
+                                {{--                                <td>{!! $poet->price !!}</td>--}}
+                                {{--                                <td>{!! $poet->price_daily !!}</td>--}}
+                                {{--                                <td>{!! $poet->page !!}</td>--}}
+                                <td>{!! $poet->count !!}</td>
+                                <td>{!! $poet->sell_count !!}</td>
+                                <td class="col-2">
+                                    <form action="{{ route('admin.books.destroy',$poet->id) }}" method="POST"
+                                          onSubmit="return confirm('Rostan ham o`chirilishini hohlaysizmi?');">
+
+                                        <a class="btn btn-warning btn-sm"
+                                           href="{{ route('admin.books.edit',$poet->id) }}">
+                                            <span class="btn-label">
+                                                <i class="fa fa-pen"></i>
+                                            </span>
+
+                                        </a>
+
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger btn-sm">
+                                            <span class="btn-label">
+                                                <i class="fa fa-trash"></i>
+                                            </span>
+                                        </button>
+                                    </form>
+
+                                </td>
+                            </tr>
+                        @endforeach
+
+                        </tbody>
+
+                    </table>
+
+                    <div class="container">
+                        <div class="row justify-content-center">
+
+                            @if ($books->links())
+                                <div class="mt-4 p-4 box has-text-centered">
+                                    {{ $books->links() }}
+                                </div>
+                            @endif
+
+                        </div>
+                    </div>
+
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
 
 
 @endsection
--- resources/views/User/contact.blade.php
+++ resources/views/User/contact.blade.php
@@ -28,16 +28,10 @@
 
                 <div class="col-md-12">
                     <div class="section-heading">
-                        <h2>Our Location on Maps</h2>
+                        <h2>Bizning manzilimiz</h2>
                     </div>
                 </div>
                 <div class="col-md-8">
-                    <!-- How to change your own map point
-                        1. Go to Google Maps
-                        2. Click on your location point
-                        3. Click "Share" and choose "Embed map" tab
-                        4. Copy only URL and paste it within the src="" field below
-                    -->
                     <div id="map">
                         <iframe
                             src="https://maps.google.com/maps?q=Av.+L%C3%BAcio+Costa,+Rio+de+Janeiro+-+RJ,+Brazil&t=&z=13&ie=UTF8&iwloc=&output=embed"
@@ -46,14 +40,12 @@
                 </div>
                 <div class="col-md-4">
                     <div class="left-content">
-                        <h4>About our office</h4>
+                        <h4>Biz haqimizda</h4>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisic elit. Sed voluptate nihil eumester
                             consectetur similiqu consectetur.<br><br>Lorem ipsum dolor sit amet, consectetur adipisic
                             elit. Et, consequuntur, modi mollitia corporis ipsa voluptate corrupti.</p>
                         <ul class="social-icons">
                             <li><a href="#"><i class="fa fa-telegram"></i></a></li>
-                            <li><span class="fa fa-telegram">&nbsp;</span>
-                            </li>
                             <li><a href="#"><i class="fa fa-instagram"></i></a></li>
                             <li><a href="#"><i class="fa fa-youtube"></i></a></li>
                         </ul>
