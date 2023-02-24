5bec7f4b5de1fef9bf744eefba05412154cd55fc
Ozodbek <ozodbekozodov0002@gmail.com>
2023-02-24 23:22:52 +0500

    ...

--- resources/views/Admin/master.blade.php
+++ resources/views/Admin/master.blade.php
@@ -144,10 +144,13 @@
 {{--Bootstrap--}}
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
         integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
-        crossorigin="anonymous"></script>
+        crossorigin="anonymous">
+</script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
         integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
-        crossorigin="anonymous"></script>
+        crossorigin="anonymous">
+
+</script>
 
 
 <script>
@@ -171,7 +174,7 @@
         text: '{{ session('msg') }}',
     });
     @endif
-        
+
     $('.show_confirm').click(function (event) {
         var form = $(this).closest("form");
         var name = $(this).data("name");
