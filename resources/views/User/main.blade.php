5c8cd5b2cb52435ded23f5e0704d049d8221a753
Ozodbek <ozodbekozodov0002@gmail.com>
2023-03-03 16:17:38 +0500

    progressing

--- resources/views/User/main.blade.php
+++ resources/views/User/main.blade.php
@@ -79,24 +79,22 @@
                 </div>
                 <div class="col-md-6">
                     <div class="left-content">
-                        <h4>Looking for the best products?</h4>
-                        <p><a rel="nofollow" href="https://templatemo.com/tm-546-sixteen-clothing" target="_parent">This
-                                template</a> is free to use for your business websites. However, you have no permission
-                            to redistribute the downloadable ZIP file on any template collection website. <a
-                                rel="nofollow" href="https://templatemo.com/contact">Contact us</a> for more info.</p>
+                        <h4>Siz sifatli va hamyonbob kitoblarni qidiryapsizmi?</h4>
+                        <p>
+                            Biz sizga eng yaxshi kitoblarni taklif qilamiz. Bizning kitoblarni o'zingizga moslashtirib, sizga
+                            kerakli kitoblarni tanlang. Bizning kitoblarni sizga eng yaxshi narxga sotib olishingiz mumkin.
+                        </p>
                         <ul class="featured-list">
-                            <li><a href="#">Lorem ipsum dolor sit amet</a></li>
-                            <li><a href="#">Consectetur an adipisicing elit</a></li>
-                            <li><a href="#">It aquecorporis nulla aspernatur</a></li>
-                            <li><a href="#">Corporis, omnis doloremque</a></li>
-                            <li><a href="#">Non cum id reprehenderit</a></li>
+                            <li><a href="#"> Halollik foydadan ustun</a></li>
+                            <li><a href="#">Sifat & hamyonbob</a></li>
+                            <li><a href="#">Tez & Ishonchli </a></li>
+                            <li><a href="#"> Yetkazib berish hizmati </a></li>
                         </ul>
-                        <a href="about.html" class="filled-button">Read More</a>
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="right-image">
-                        <img src="{{asset('user/assets/images/feature-image.jpg')}}" alt="">
+                        <img src="{{asset('user/assets/images/books.png')}}" alt="">
                     </div>
                 </div>
             </div>
--- resources/views/User/main/footer.blade.php
+++ resources/views/User/main/footer.blade.php
@@ -5,7 +5,7 @@
             <div class="col-md-12">
                 <div class="inner-content">
                     <p>Copyright &copy; 2022 Online Library.
-                        - Design: <a rel="nofollow noopener" href="https://t.me/Ozodbek_Ozodov2" target="_blank">Ozodbek Ozodov</a>
+                        - Design: <a rel="nofollow noopener" href="http://ozodov.uz" target="_blank">Ozodbek Ozodov</a>
                     </p>
                 </div>
             </div>
