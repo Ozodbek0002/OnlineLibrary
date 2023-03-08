55d7969150d437dfdc69094c0dcae5329e5fbab8
Ozodbek <ozodbekozodov0002@gmail.com>
2023-03-08 23:40:24 +0500

    progressing

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,6 +15,35 @@ class DashboardController extends Controller
         $books_price = Order::where('is_paid',1)->sum('price');
         $books_count = Book::all()->count();
         $all_books_count = Book::all()->sum('count');
+        $months = [
+            'Yan',
+            'Fev',
+            'Mar',
+            'Apr',
+            'May',
+            'Iyun',
+            'Iyul',
+            'Avg',
+            'Sen',
+            'Okt',
+            'Noy',
+            'Dek',
+        ];
+        $rent_monthly = [];
+        $sell_monthly = [];
+        $rent_count = 0;
+
+        for ($i=1; $i<=12; $i++){
+           $rent_monthly[$i] = Order::whereMonth('created_at', $i)->where('busy_id',2)->sum('count');
+        }
+        for ($i=1; $i<=12; $i++){
+            $sell_monthly[$i] = Order::whereMonth('created_at', $i)->where('busy_id',1)->sum('count');
+        }
+
+        $rent_monthly = array_values($rent_monthly);
+        $sell_monthly = array_values($sell_monthly);
+
+
 
         return view('admin.dashboard',[
             'sell_books_count'=>$sell_books_count,
@@ -22,6 +51,9 @@ class DashboardController extends Controller
             'books_price'=>$books_price,
             'books_count'=>$books_count,
             'all_books_count'=>$all_books_count,
+            'months'=>$months,
+            'rent_monthly'=>$rent_monthly,
+            'sell_monthly'=>$sell_monthly,
         ]);
     }
 }
