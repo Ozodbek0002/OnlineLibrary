2cf170f3c9226410e00092bb35107fc739c8e560
Ozodbek <ozodbekozodov0002@gmail.com>
2023-10-03 07:31:14 +0500

    fixed

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -21,45 +21,7 @@ class ClientController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create(): Response
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request): RedirectResponse
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id): Response
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id): Response
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id): RedirectResponse
-    {
-        //
-    }
 
     /**
      * Remove the specified resource from storage.
